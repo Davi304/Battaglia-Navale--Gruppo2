48bcba325cde2f1a5e1e2e9a07dc764ad93f2e75
Davi304 <123454837+Davi304@users.noreply.github.com>
2023-01-27 14:49:26 +0100

    Add files via upload

---
+++ Battaglia_Navale.java
@@ -0,0 +1,10 @@
+public class Battaglia_Navale 
+{
+    public static void main(String[] args) throws Exception 
+    {
+        //Il main richiama soltanto la classe principale Gioco
+
+        Gioco gioco = new Gioco();
+
+    }
+}
