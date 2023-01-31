09e619c560af5375548f92ebbb3ea0cdea0baaab
gabriele301 <nerigabriele05@gmail.com>
2023-01-31 19:45:36 +0100

    classe nave

--- Nave.java
+++ Nave.java
@@ -10,67 +10,72 @@ public class Nave
     boolean orizzontale=true; // se true è orizzontale se false è verticale
 
 
-    public Nave(int posizioneX[], int posizioneY[], int grandezza, boolean orizontale)
+    public Nave(int posizioneX[], int posizioneY[], int grandezza, boolean orizontale)// costruttore di nave
     {
 
         this.orizzontale=orizontale;
         this.grandezza=grandezza;
 
-        if ( orizzontale= true){
+        if ( orizzontale= true)
+        {// in caso sia orizzontale allora entra nel if 
             posizioneX= new int [grandezza];
             posizioneY= new int [1];
         }
 
-        else{
+        else{// in caaso sia verticale entra nell'else
             posizioneX= new int [1];
             posizioneY= new int [grandezza];
         }
 
-        this.posizioneX=posizioneX;
+        this.posizioneX=posizioneX;//dopo aver creato gli spazi per i dati della posizione li riempiamo
         this.posizioneY=posizioneY;
     }
 
-    public boolean colpito(int x, int y){
+    public boolean colpito(int x, int y)
+    {// controlla se la nave è stata colpita
        
-        if (orizzontale=true){
+        if (orizzontale=true)
+        {// se orizzontale la y sarà una e la x sarà >1
 
-            if (posizioneY[0]==y){
+            if (posizioneY[0]==y)
+            {
 
-                for (int i =0; i < grandezza; i ++){
+                for (int i =0; i < grandezza; i ++)
+                {
 
-                    if (posizioneX[i]==x){
+                    if (posizioneX[i]==x)
+                    {
                         return true;
                     }
                     
                 }
             }
-            else{
+            else
+            {
                 return false;
             }
             return false;
         }
 
-        else {
+        else 
+        {// il contrario di sopra 
 
-            if (posizioneX[0]==x){
+            if (posizioneX[0]==x)
+            {
 
-                for (int j =0; j < grandezza; j ++){
-
-                    if (posizioneY[j]==y){
+                for (int j =0; j < grandezza; j ++)
+                {
+                    if (posizioneY[j]==y)
+                    {
                         return true;
                     }
-                    
                 }
             }
-            else{
+            else
+            {
                 return false;
             }
             return false;
         }
-       
     }
-
-
-
-
 }
