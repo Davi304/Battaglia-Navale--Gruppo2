0d51de56d127a1f9b5765aa326fc79cc54e2de0a
Davi304 <davide.ferrario304@gmail.com>
2023-01-31 11:01:03 +0100

    gioco e griglia

--- Gioco.java
+++ Gioco.java
@@ -1,9 +1,18 @@
 public class Gioco 
 {
     //La classe gioco inizializza la griglia e l'array di navi e gestisce tutto il gioco
+    //forse la classe gioco è inutile
+    Nave navi[] = new Nave[5];
+    //Griglia griglia = new Griglia();
+
 
     public Gioco()
     {
-        //prova github
+
+
+        for(int i=0; i<5; i++)
+        {
+            //navi[i] = new Nave()
+        }
     }
 }
--- Griglia.java
+++ Griglia.java
@@ -6,24 +6,29 @@ public class Griglia extends JFrame implements ActionListener
 {
     //La classe griglia crea la griglia di gioco con le matrici che stava facendo Andrea
     //
+    JPanel panel = new JPanel();
+    JPanel[][] caselle = new JPanel[11][11];
+    Nave navi[] = new Nave[5];
 
     public Griglia()
     {
         // per fare la griglia usare GridLayout di 11x11
         //aggiungere la scacchiera a GridBagLayout e scrivere no GridBagContraints
 
-        /*
-         p.setLayout(new GridLayout(11,11));
+        panel.setLayout(new GridLayout(11,11));
 
         for(int i=0; i<11; i++)
         {
             for(int j=0; j<11; j++)
             {
                 caselle[i][j] = new JPanel();
-                p.add(caselle[i][j]);
+                panel.add(caselle[i][j]);
             }
         }
-         */
+
+        
+
+         
     }
 
     public void actionPerformed(ActionEvent e) 
