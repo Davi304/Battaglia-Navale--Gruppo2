6633145a7eb86bfb8901ce8f658dd2d2825e94c8
Davi304 <davide.ferrario304@gmail.com>
2023-02-08 10:43:18 +0100

    classe bottoni per fare cose dopo

--- Bottoni.java
+++ Bottoni.java
@@ -1,15 +1,27 @@
-import javax.swing.JButton;
-import java.awt.event.*;
+import javax.swing.*;
 
-public class Bottoni extends JButton implements ActionListener
+public class Bottoni extends JButton
 {
-    //La classe bottoni la usaimo per creare la griglia di bottoni
-   
+    //La classe bottoni la usiamo per creare la griglia di bottoni
+    
+    int x;
+    int y;
 
+    public Bottoni(int x, int y, ImageIcon icona) //crea i bottoni del mare
+    {
+        this.x = x;
+        this.y = y;
 
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
-        
+        this.setIcon(icona);
     }
+
+    public Bottoni(String testo) //crea i bottoni del testo
+    {
+        this.x = 11; //lo mettiamo a 11 perchè non serve per le navi
+        this.y = 11;
+
+        this.setText(testo);
+    }
+
     
 }
