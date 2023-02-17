6a45c5e5c7dde12f711375a06da820e52355facf
AndySala7 <sala.official77@gmail.com>
2023-02-17 08:38:58 +0100

    Finti progressi

--- Vittoria_sconfitta.java
+++ Vittoria_sconfitta.java
@@ -5,32 +5,31 @@ public class Vittoria_sconfitta extends JFrame
 {
     
     JPanel testoRisultato = new JPanel();
-    JLabel info = new JLabel();
+    JLabel infoVittoria = new JLabel();
+    JLabel info = new JLabel(new ImageIcon("Immagini/GAME-OVER.jpg"));
     Font f = new Font("Impact", Font.CENTER_BASELINE,60);
 
     public Vittoria_sconfitta(boolean vittoria_sconfitta)
     {
         if(vittoria_sconfitta==true)
         {
+            testoRisultato.setBackground(new Color(0,0,0));
+            testoRisultato.add(infoVittoria,BorderLayout.CENTER);
             setTitle("Vittoria");
-            info.setFont(f);
-            info.setForeground(Color.YELLOW);
-            info.setText("HAI VINTO !!!");
+            infoVittoria.setFont(f);
+            infoVittoria.setForeground(Color.YELLOW);
+            infoVittoria.setText("HAI VINTO !!!");
         }
         
         else if(vittoria_sconfitta==false)
         {
-            setTitle("Sconfitta");
-            info.setFont(f);
-            info.setForeground(Color.RED);
-            info.setText("...GAME OVER...");
+            testoRisultato.add(info,BorderLayout.CENTER);
+            info.setIcon(new ImageIcon("Immagini/GAME-OVER.jpg"));
         }
         
 
         add(testoRisultato,BorderLayout.CENTER);
         setSize(400,300);
-        testoRisultato.setBackground(new Color(0,0,0));
-        testoRisultato.add(info,BorderLayout.CENTER);
         setVisible(true);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
